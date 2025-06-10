57656f12ab220e410a779c60fc62b99a468c4995
nhattruong022 <nhattruongp78@gmail.com>
2025-06-10 10:30:11 +0700

    fix url image

--- Lecture_web/obj/Debug/net8.0/Lecture_web.AssemblyInfo.cs
+++ Lecture_web/obj/Debug/net8.0/Lecture_web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lecture_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da4c650f03dc202f80bbd099e560f6d957514b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a688f7127f2045f12996dca93719facba8468de4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lecture_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lecture_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
